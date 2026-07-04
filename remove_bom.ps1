701c377cd5e0c72799775fe3580ebad4ad1d7cfa
yushen1202 <yushen1202@hotmail.com>
2026-07-04 11:47:50 +0800

    commit

--- remove_bom.ps1
+++
@@ -1,8 +0,0 @@
-﻿Get-ChildItem -Path "d:\workspace\workspace_tech_yushen1202\tech-mq-rocketmq" -Recurse -Filter "*.java" | ForEach-Object {
-    $bytes = [System.IO.File]::ReadAllBytes($_.FullName)
-    if ($bytes.Length -ge 3 -and $bytes[0] -eq 0xEF -and $bytes[1] -eq 0xBB -and $bytes[2] -eq 0xBF) {
-        [System.IO.File]::WriteAllBytes($_.FullName, $bytes[3..($bytes.Length-1)])
-        Write-Host "Removed BOM: $($_.FullName)"
-    }
-}
-Write-Host "Done"
--- remove_bom_single.ps1
+++
@@ -1,8 +0,0 @@
-$filePath = "d:\workspace\workspace_tech_yushen1202\tech-mq-rocketmq\rocketmq-demo\src\main\java\com\example\rocketmq\controller\MessageController.java"
-$bytes = [System.IO.File]::ReadAllBytes($filePath)
-if ($bytes.Length -ge 3 -and $bytes[0] -eq 0xEF -and $bytes[1] -eq 0xBB -and $bytes[2] -eq 0xBF) {
-    [System.IO.File]::WriteAllBytes($filePath, $bytes[3..($bytes.Length-1)])
-    Write-Host "Removed BOM"
-} else {
-    Write-Host "No BOM found"
-}
